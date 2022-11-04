8c3e004aa50b0fd80736d244f019abccbc5a7793
codethrx <tatheermehdi2000@gmail.com>
2022-11-04 23:01:06 +0500

    adding server file

--- core/frontend/src/App.jsx
+++ core/frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import "./App.css";
-import { successMsg } from "./libs/reusables/Toast";
-import TodoList from "./components/TodoList";
-import TodoInput from "./components/TodoInput";
-import { useTodoCtx } from "./context/TodoContext";
+// import { successMsg } from "./libs/reusables/Toast";
+// import TodoList from "./components/TodoList";
+// import TodoInput from "./components/TodoInput";
+// import { useTodoCtx } from "./context/TodoContext";
+import { gql } from "@apollo/client/core";
+import { useQuery } from "@apollo/client";
 function App() {
-  const { data, addTodos } = useTodoCtx();
+  // const { data, addTodos } = useTodoCtx();
+  // console.log(data);
+  const GET_GEN_3 = gql`
+    {
+      posts {
+        title
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(GET_GEN_3);
   console.log(data);
   return (
     <div className="bg-[#212121] min-h-[100vh]  flex items-center flex-col pt-5">
-      <TodoInput addTodos={addTodos} successMsg={successMsg} />
-      <TodoList data={data} />
+      {/* <TodoInput addTodos={addTodos} successMsg={successMsg} />
+      <TodoList data={data} /> */}
+      <h1>Exploring GraphQl..</h1>
     </div>
   );
 }
--- core/frontend/src/main.jsx
+++ core/frontend/src/main.jsx
@@ -5,11 +5,19 @@ import "./index.css";
 import { TodoCtxProvider } from "./context/TodoContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+const client = new ApolloClient({
+  // uri: "https://beta.pokeapi.co/graphql/v1beta",
+  uri: "https://jsonplaceholder.typicode.com",
+  cache: new InMemoryCache(),
+});
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TodoCtxProvider>
-      <App />
-      <ToastContainer />
-    </TodoCtxProvider>
+    <ApolloProvider client={client}>
+      <TodoCtxProvider>
+        <App />
+        <ToastContainer />
+      </TodoCtxProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
